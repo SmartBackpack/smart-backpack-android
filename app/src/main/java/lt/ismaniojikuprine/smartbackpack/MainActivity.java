13947bf62b9a0f5695da607acb84154b8705a0c5
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2017-03-04 12:22:40 +0200

    Created data send and receive

--- app/src/main/java/lt/ismaniojikuprine/smartbackpack/MainActivity.java
+++ app/src/main/java/lt/ismaniojikuprine/smartbackpack/MainActivity.java
@@ -11,9 +11,8 @@ import android.widget.Toast;
 
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 import app.akexorcist.bluetotohspp.library.BluetoothState;
-import app.akexorcist.bluetotohspp.library.DeviceList;
 
-public class MainActivity extends AppCompatActivity implements BluetoothSPP.OnDataReceivedListener, BluetoothSPP.BluetoothStateListener, BluetoothSPP.BluetoothConnectionListener {
+public class MainActivity extends AppCompatActivity implements BluetoothSPP.OnDataReceivedListener, BluetoothSPP.BluetoothStateListener {
 
     private BluetoothSPP bluetooth;
 
@@ -25,16 +24,19 @@ public class MainActivity extends AppCompatActivity implements BluetoothSPP.OnDa
         Button connect = (Button) findViewById(R.id.connect);
         connect.setOnClickListener(this::onConnectButtonClicked);
 
+        Button send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(this::onSendButtonClicked);
+
         bluetooth = new BluetoothSPP(this);
         bluetooth.setBluetoothStateListener(this);
-        bluetooth.setBluetoothConnectionListener(this);
         bluetooth.setOnDataReceivedListener(this);
     }
 
     private void onConnectButtonClicked(View v) {
         if (bluetooth.isBluetoothEnabled()) {
-            Intent intent = new Intent(this, DeviceList.class);
-            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+            bluetooth.setupService();
+            bluetooth.startService(BluetoothState.DEVICE_OTHER);
+            bluetooth.autoConnect(getString(R.string.device_name));
         } else {
             Toast.makeText(this, R.string.enable_bluetooth, Toast.LENGTH_SHORT).show();
             openBluetoothSettings();
@@ -47,52 +49,29 @@ public class MainActivity extends AppCompatActivity implements BluetoothSPP.OnDa
         startActivity(intentBluetooth);
     }
 
+    private void onSendButtonClicked(View view) {
+        bluetooth.send("HELLO WORLD", true);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE && resultCode == Activity.RESULT_OK) {
-            bluetooth.setupService();
-            bluetooth.startService(BluetoothState.DEVICE_OTHER);
             bluetooth.connect(data);
         }
     }
 
     @Override
     public void onDataReceived(byte[] data, String message) {
-        Log.e("Received", "Received");
+        Log.e("Received", message);
     }
 
     @Override
     public void onServiceStateChanged(int state) {
         if (state == BluetoothState.STATE_CONNECTED) {
-            Log.e("STATE", "CONNECTED");
+            Toast.makeText(this, R.string.connected, Toast.LENGTH_SHORT).show();
         }
         else if (state == BluetoothState.STATE_CONNECTING) {
             Toast.makeText(this, R.string.connecting, Toast.LENGTH_SHORT).show();
-            Log.e("STATE", "CONNECTING");
         }
-        else if (state == BluetoothState.STATE_LISTEN) {
-            Log.e("STATE", "LISTEN");
-        }
-        else if (state == BluetoothState.STATE_NONE) {
-            Log.e("STATE", "NONE");
-        }
-    }
-
-    @Override
-    public void onDeviceConnected(String name, String address) {
-        Log.e("DEVICE", "CONNECTED");
-        Toast.makeText(this, R.string.connected, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onDeviceDisconnected() {
-        Log.e("DEVICE", "DISCONNECTED");
-        Toast.makeText(this, R.string.disconnected, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onDeviceConnectionFailed() {
-        Log.e("DEVICE", "CONNECTION FAILED");
-        Toast.makeText(this, R.string.connection_failed, Toast.LENGTH_SHORT).show();
     }
 }
